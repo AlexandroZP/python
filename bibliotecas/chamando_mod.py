195bea992ff0676e8247ee94d83fe87a46550702
Alexandro Zava Pereira <alexandrozava@gmail.com>
2022-03-23 15:05:55 -0300

    modulos

---
+++ bibliotecas/chamando_mod.py
@@ -0,0 +1,5 @@
+import mod_circulo;
+
+print (mod_circulo.Pi);
+print(mod_circulo.area(5));
+print(mod_circulo.comprimento_circunferencia(5));
---
+++ bibliotecas/mod_circulo.py
@@ -0,0 +1,7 @@
+Pi = 3.14159;
+
+def area(raio):
+    return Pi * (raio ** 2);
+
+def comprimento_circunferencia(raio):
+    return 2 * Pi * raio;

2ee64bb3fb036ab36e33c42afd4ad11da644cf69
Alexandro Zava <alexandrozava@gmail.com>
2022-04-21 18:05:40 -0300

    Aula011-cores

---
+++ Curso_em_video/Mundo_1/teste_aula11.py
@@ -0,0 +1,8 @@
+print('\033[1;30;41m Teste \033[m')
+print('\033[4;33;44m Teste \033[m')
+print('\033[1;35;43m Teste \033[m')
+print('\033[0;30;42m Teste \033[m')
+print('\033[m Teste')
+print('\033[7;30m Teste \033[m')
+nome = 'Alexandro'
+print('Olá {}{}{}'.format('\033[;34m', nome, '\033[m'))

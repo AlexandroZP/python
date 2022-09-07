cc11dc8aea3207babda5b0302a9a32af68c69a2a
Alexandro Zava Pereira <alexandrozava@gmail.com>
2022-09-07 18:33:15 -0300

    ex113 - 115

--- Curso_em_video/Mundo_3/exercicios/ex108/main.py
+++
@@ -1,8 +0,0 @@
-import moeda
-
-valor = float(input('Digite o valor R$'))
-
-print(f'A metade de {moeda.moeda(valor)} é {moeda.metade(valor, True)}')
-print(f'O dobro de {moeda.moeda(valor)} é {moeda.dobro(valor, True)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(10, valor, True)}')
-print(f'diminuindo 13%, temos {moeda.diminuir(13, valor, True)}')
